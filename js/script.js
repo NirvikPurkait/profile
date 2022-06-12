59365c216e20a065a2c98f4524404483bd068bb1
NirvikPurkait <93871471+NirvikPurkait@users.noreply.github.com>
2022-06-12 16:18:16 +0530

    started from scratch

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function changingNavSection() {
+    
+    let toChange = document.getElementsByClassName('navListSm');
+
+    let toCross = document.getElementsByClassName('hamburger');
+
+    if (toChange[0].style.display == 'flex') {
+        toChange[0].style.display = 'none'
+    }
+    else {
+        toChange[0].style.display = 'flex'
+    }
+
+    toCross[0].classList.toggle('cross');
+
+}
