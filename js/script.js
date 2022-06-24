4f477ba4459d532c1c8ad5ba7d948acdd52708ad
NirvikPurkait <93871471+NirvikPurkait@users.noreply.github.com>
2022-06-24 21:26:35 +0530

    added hover effect but still not able to do as i desire

--- js/script.js
+++ js/script.js
@@ -23,50 +23,72 @@ function changingNavSection() {
 // this function is to showing and hiding each dropdown on navbar
 
 
-var dropdownList = document.getElementsByClassName('dropdownList');
+var dropdown = document.getElementsByClassName('dropdown');
 // gets the dropdown list element
 
-var dropdownList1 = dropdownList[0];
-var dropdownList2 = dropdownList[1];
-var dropdownList3 = dropdownList[2];
+var dropdown1 = dropdown[0];
+var dropdown2 = dropdown[1];
+var dropdown3 = dropdown[2];
 // gets the each dropdown list
 
+function displayingAboutDropdownHover() {
+    dropdown1.style.display = 'block'
+}
 
 function displayingAboutDropdown() {
+    if (dropdown1.style.display == 'none') {
+        dropdown1.style.display = 'block'
+    }
+    else {
+        dropdown1.style.display = 'none'
+    }
+}
 
-    dropdownList1.classList.toggle('hide')
+function hidingAboutDropdown() {
+    dropdown1.style.display = 'none'
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+function displayingWorkDropdownHover() {
+    dropdown2.style.display = 'block'
+}
 
 function displayingWorkDropdown() {
-
-    if (dropdownList2.style.display == 'block') {
-        dropdownList2.style.display = 'none';
+    if (dropdown2.style.display == 'none') {
+        dropdown2.style.display = 'block'
     }
     else {
-        dropdownList2.style.display = 'block'
+        dropdown2.style.display = 'none'
     }
 }
+
+function hidingWorkDropdown() {
+    dropdown2.style.display = 'none'
+}
+
+
+function displayingGalaryDropdownHover() {
+    dropdown3.style.display = 'block'
+}
+
 function displayingGalaryDropdown() {
-
-    if (dropdownList3.style.display == 'block') {
-        dropdownList3.style.display = 'none';
+    if (dropdown3.style.display == 'none') {
+        dropdown3.style.display = 'block'
     }
     else {
-        dropdownList3.style.display = 'block'
+        dropdown3.style.display = 'none'
     }
 }
+
+function hidingGalaryDropdown() {
+    dropdown3.style.display = 'none'
+}
+
+
+
+
+
+
+
+
